c51a9fcd4f33eaf177e6b6948e5a2bbf4fb91040
iampz <i@iampz.com>
2020-01-22 00:31:17 +0700

    Add LIVE feature and change some content and UI

--- js/live.js
+++ js/live.js
@@ -1,8 +1,20 @@
 const device = getDevice();
 const anchor = document.getElementById('facebook-live');
+const thumb = document.getElementById('live-thumbnail');
+const wording = document.getElementById('live-wording');
 const live_id = new URLSearchParams(location.search).get('id');
+
 if (live_id) {
-  if (device === 'computer') anchor.href += live_id;
+
+  wording.textContent = 'กดเพื่อเข้ารับชม LIVE';
+  thumb.src = 'http://api.shophil.com/facebook-video-thumb/' + live_id;
+
+  if (device === 'computer') anchor.href = 'https://www.facebook.com/GirlyDesire/videos/' + live_id;
   else if (device === 'android') anchor.href = 'fb://fullscreen_video_redirect/' + live_id;
   else if (device === 'ios') anchor.href = 'fb://fullscreen_video/' + live_id;
+
+} else if (device !== 'computer') {
+
+  anchor.href = 'fb-messenger://user/379172625480987?ref=live-notification';
+
 }
